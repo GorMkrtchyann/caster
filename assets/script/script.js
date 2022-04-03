0f233c6d73e9dfc71dfce1fb561a92eadf43e87a
GorMkrtchyann <97087463+GorMkrtchyann@users.noreply.github.com>
2022-04-03 13:03:12 +0400

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,15 @@
 document.querySelector('.open-arrow').addEventListener("click", function(){
     document.querySelector('.menu').classList.toggle('menu-active');
 });
+window.addEventListener("scroll", function(){
+    document.querySelector('.menu').classList.remove('menu-active');
+})
+
+document.querySelector('#code-but').addEventListener("click", function(){
+    document.querySelector('.ul').classList.toggle('ul-active')
+});
+document.querySelector('#code-but-main').addEventListener("click", function(){
+    document.querySelector('.ul').classList.toggle('ul-active')
+    document.querySelector('.menu').classList.toggle('menu-active');
+
+});
