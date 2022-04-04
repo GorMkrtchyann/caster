1df9f9a4d2f250b32a5e30168fd0c954ee8a8229
GorMkrtchyan <gormkrtchyann@gmail.com>
2022-04-04 20:03:06 +0400

    caster page finish

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,5 +11,20 @@ document.querySelector('#code-but').addEventListener("click", function(){
 document.querySelector('#code-but-main').addEventListener("click", function(){
     document.querySelector('.ul').classList.toggle('ul-active')
     document.querySelector('.menu').classList.toggle('menu-active');
+});
 
+let cssCdnBut = document.querySelector('#css-copy');
+let cssCdn = document.querySelector('#css-cdn').textContent;
+
+cssCdnBut.addEventListener("click", function(){
+    navigator.clipboard.writeText(cssCdn)
+    document.querySelector('.cdn i').style.display = 'inline'
+});
+
+let jsCdnBut = document.querySelector('#js-copy');
+let jsCdn = document.querySelector('#js-cdn').textContent;
+
+jsCdnBut.addEventListener("click", function(){
+    navigator.clipboard.writeText(jsCdn)
+    document.querySelector('.cdn2 i').style.display = 'inline'
 });
